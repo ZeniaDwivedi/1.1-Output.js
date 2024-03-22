e9e0efc6b0995eb0b564729e5ab66318a998b13a
ZeniaDwivedi <159184288+ZeniaDwivedi@users.noreply.github.com>
2024-03-21 21:17:26 -0400

    Create 1.1-Output.js

---
+++ 1.1-Output.js
@@ -0,0 +1,50 @@
+1. Add the appropriate code(s) to the JavaScript file 1.1-Output.js that will print the address for Apple Inc.:
+Apple Inc.
+One Apple Park Way
+Cupertino, CA
+95014
++1 408-996-1010
+
+1.1-Output.js
+
+Task 1: Print the address for Apple Inc.
+console.log("Apple Inc.");
+console.log("One Apple Park Way");
+console.log("Cupertino, CA");
+console.log("95014");
+console.log("+1 408-996-1010");
+
+
+2. Output two blank lines (to separate task 1 and 3)
+
+console.log("\n\n");
+
+
+
+3. Now print the following with only a SINGLE console.log( ) command:
+Microsoft Corporation
+One Microsoft Way
+Redmond, WA
+98052
++1 425-882-8080
+
+Task 3: Print the address for Microsoft Corporation with a single console.log() command
+console.log("Microsoft Corporation\nOne Microsoft Way\nRedmond, WA\n98052\n+1 425-882-8080");
+
+
+
+4. Output two blank lines (to separate task 3 and 5)
+
+Task 4: Output two blank lines
+console.log("\n\n");
+
+
+5. Finally, output the following to the console using one console.log( ) command and horizontal tabs between the letters:
+x  o  x
+o  x  o
+x  o  x
+
+console.log("x\to\tx");
+console.log("o\tx\to");
+console.log("x\to\tx");
+
