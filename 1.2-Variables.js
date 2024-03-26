6a72639bc643a5ec5d63935bdb56c5b1fdac3e36
ZeniaDwivedi <159184288+ZeniaDwivedi@users.noreply.github.com>
2024-03-25 22:23:35 -0400

    Create  1.2-Variables.js

---
+++ 1.2-Variables.js
@@ -0,0 +1,14 @@
+// Part 1
+let length = 10;
+let width = 5;
+let area = length * width;
+
+console.log(area);
+console.log("Area: " + area);
+
+// Part 2
+let first_name = "Santa";
+let last_name = "Claws";
+last_name = "Claus";
+
+console.log("I still believe in " + first_name + " " + last_name + ".");
